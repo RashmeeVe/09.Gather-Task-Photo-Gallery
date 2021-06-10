74992a1346581332aea753180a2644251026c6cb
RashmeeVe <83055419+RashmeeVe@users.noreply.github.com>
2021-06-10 19:08:49 +0530

    Add files via upload

---
+++ src/MyStyles.js
@@ -0,0 +1,200 @@
+const styles = {
+  mainDiv: {
+    background: "linear-gradient(#fff, #f2f2f2)",
+    height: "auto",
+    width: "100%",
+    position: "absolute",
+    textAlign: "center",
+    minWidth: 1280,
+  },
+
+  galleryOuterDiv: {
+    width: "auto",
+    height: "auto",
+  },
+
+  gallery: {
+    display: "flex",
+    justifyContent: "center",
+    marginBottom: 20,
+  },
+
+  card: {
+    position: "relative",
+    height: "100%",
+    // width: "fit-content",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    cursor: "pointer",
+    zIndex: 1,
+  },
+
+  verticalString: {
+    width: 1,
+    background: "linear-gradient(#fff 0%, #000 70%)",
+  },
+  clipCircle: {
+    width: 10,
+    border: "1px solid #000",
+    height: 10,
+    zIndex: 2,
+    borderRadius: "50%",
+  },
+  clipRectangle: {
+    width: 30,
+    height: 8,
+    zIndex: 2,
+    background: "#000",
+  },
+  imageDivBorder: {
+    border: "2px solid white",
+    boxShadow: "0px 0px 60px 0px #dad9d9",
+  },
+  imageCSS: {
+    height: "100%",
+    width: "100%",
+    cursor: "pointer",
+  },
+  //Card1 Starts
+  // card1: { left: 10, top: 150 },
+  card1: { marginLeft: "2px", top: 50 },
+  verticalString1: { height: 100 },
+  imageDiv1: {
+    width: 100,
+    height: 100,
+    zIndex: 2,
+    boxShadow: "0px 0px 60px 0px #dad9d9",
+  },
+  input: {
+    display: "none",
+  },
+  button: {
+    width: "100%",
+    height: "100%",
+    lineHeight: "1.3",
+    padding: "0px 12% 0px 12%",
+    textTransform: "none",
+    borderRadius: "4px",
+  },
+  btnIcon: {
+    // margin: "5px 5px 0px 5px",
+    fontSize: 35,
+  },
+  //Card1 Ends
+
+  //Card2 Starts
+  card2: { marginLeft: "-20px" },
+  verticalString2: { height: 130 },
+  imageDiv2: {
+    width: 180,
+    height: 230,
+    zIndex: 1,
+    background: "#fca",
+  },
+  //Card2 Ends
+
+  //Card3 Starts
+  card3: { marginLeft: "-40px" },
+  verticalString3: { height: 300 },
+  imageDiv3: {
+    width: 180,
+    height: 120,
+    zIndex: 1,
+    background: "#99ebff",
+  },
+  //Card3 Ends
+
+  //Card4 Starts
+  card4: { marginLeft: "-130px" },
+  verticalString4: { height: 50 },
+  imageDiv4: {
+    width: 170,
+    height: 170,
+    zIndex: 1,
+    background: "#ffcce6",
+  },
+  //Card4 Ends
+
+  //Card5 Starts
+  card5: { marginLeft: "-20px" },
+  verticalString5: { height: 80 },
+  imageDiv5: {
+    width: 200,
+    height: 280,
+    background: "#ffc2b3",
+  },
+  //Card5 Ends
+
+  //Card6 Starts
+  card6: { marginLeft: "-20px" },
+  verticalString6: { height: 55 },
+  imageDiv6: {
+    width: 200,
+    height: 150,
+    zIndex: 1,
+    background: "#c6ffb3",
+  },
+  //Card6 Ends
+
+  //Card7 Starts
+  card7: { marginLeft: "-110px" },
+  verticalString7: { height: 290 },
+  imageDiv7: {
+    width: 100,
+    height: 100,
+    background: "#ffccf2",
+  },
+  //Card7 Ends
+
+  //Card8 Starts
+  card8: { marginLeft: "-20px" },
+  verticalString8: { height: 30 },
+  imageDiv8: {
+    width: 120,
+    height: 220,
+    zIndex: 1,
+    background: "#ffeb99",
+  },
+  //Card8 Ends
+
+  //Card9 Starts
+  card9: { marginLeft: "-20px" },
+  verticalString9: { height: 110 },
+  imageDiv9: {
+    width: 150,
+    height: 150,
+    zIndex: 1,
+    background: "#b3b3ff",
+  },
+  //Card9 Ends
+
+  //Card10 Starts
+  card10: { top: 60, marginLeft: "-1px" },
+  verticalString10: { height: 135 },
+  imageDiv10: {
+    width: 115,
+    height: 115,
+    zIndex: 1,
+    boxShadow: "0px 0px 60px 0px #dad9d9",
+    borderRadius: "4px",
+
+    // background: "#000080",
+  },
+  //Card10 Ends
+
+  //Card11 Starts
+  card11: { top: 50, marginLeft: "-10px" },
+  verticalString11: { height: 90 },
+  imageDiv11: {
+    width: 100,
+    height: 100,
+    boxShadow: "0px 0px 60px 0px #dad9d9",
+    // background: "#000099",
+  },
+  //Card11 Ends
+};
+
+export default styles;
---
+++ src/myStyles2.js
@@ -0,0 +1,196 @@
+const styles = {
+  mainDiv: {
+    background: "linear-gradient(#fff, #f2f2f2)",
+    height: "auto",
+    width: "100%",
+    position: "absolute",
+    textAlign: "center",
+    minWidth: 1680,
+  },
+
+  galleryOuterDiv: {
+    width: "auto",
+    height: "auto",
+  },
+
+  gallery: {
+    display: "flex",
+    justifyContent: "center",
+    marginBottom: 20,
+  },
+
+  card: {
+    position: "relative",
+    height: "100%",
+    // width: "fit-content",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+
+  verticalString: {
+    width: 1,
+    background: "linear-gradient(#fff 0%, #000 70%)",
+  },
+  clipCircle: {
+    width: 10,
+    border: "1px solid #000",
+    height: 10,
+    zIndex: 2,
+    borderRadius: "50%",
+  },
+  clipRectangle: {
+    width: 30,
+    height: 8,
+    zIndex: 2,
+    background: "#000",
+  },
+  imageDivBorder: {
+    border: "2px solid white",
+    boxShadow: "0px 0px 60px 0px #dad9d9",
+  },
+  imageCSS: {
+    height: "100%",
+    width: "100%",
+    cursor: "pointer",
+  },
+  //Card1 Starts
+  // card1: { left: 10, top: 150 },
+  card1: { marginLeft: "2px", top: 50 },
+  verticalString1: { height: 100 },
+  imageDiv1: {
+    width: 115,
+    height: 115,
+    zIndex: 2,
+    boxShadow: "0px 0px 60px 0px #dad9d9",
+  },
+  input: {
+    display: "none",
+  },
+  button: {
+    width: "100%",
+    height: "100%",
+    lineHeight: "1.3",
+    padding: "0px 12% 0px 12%",
+    textTransform: "none",
+    borderRadius: "4px",
+  },
+  btnIcon: {
+    // margin: "5px 5px 0px 5px",
+    fontSize: 35,
+  },
+  //Card1 Ends
+
+  //Card2 Starts
+  card2: { marginLeft: "-20px" },
+  verticalString2: { height: 130 },
+  imageDiv2: {
+    width: 205,
+    height: 270,
+    zIndex: 1,
+    background: "#fca",
+  },
+  //Card2 Ends
+
+  //Card3 Starts
+  card3: { marginLeft: "-40px" },
+  verticalString3: { height: 340 },
+  imageDiv3: {
+    width: 195,
+    height: 120,
+    zIndex: 1,
+    background: "#99ebff",
+  },
+  //Card3 Ends
+
+  //Card4 Starts
+  card4: { marginLeft: "-145px" },
+  verticalString4: { height: 50 },
+  imageDiv4: {
+    width: 220,
+    height: 210,
+    zIndex: 1,
+    background: "#ffcce6",
+  },
+  //Card4 Ends
+
+  //Card5 Starts
+  card5: { marginLeft: "-20px" },
+  verticalString5: { height: 80 },
+  imageDiv5: {
+    width: 240,
+    height: 315,
+    background: "#ffc2b3",
+  },
+  //Card5 Ends
+
+  //Card6 Starts
+  card6: { marginLeft: "-20px" },
+  verticalString6: { height: 55 },
+  imageDiv6: {
+    width: 255,
+    height: 170,
+    zIndex: 1,
+    background: "#c6ffb3",
+  },
+  //Card6 Ends
+
+  //Card7 Starts
+  card7: { marginLeft: "-110px" },
+  verticalString7: { height: 340 },
+  imageDiv7: {
+    width: 115,
+    height: 110,
+    background: "#ffccf2",
+  },
+  //Card7 Ends
+
+  //Card8 Starts
+  card8: { marginLeft: "-20px" },
+  verticalString8: { height: 30 },
+  imageDiv8: {
+    width: 160,
+    height: 270,
+    zIndex: 1,
+    background: "#ffeb99",
+  },
+  //Card8 Ends
+
+  //Card9 Starts
+  card9: { marginLeft: "-20px" },
+  verticalString9: { height: 125 },
+  imageDiv9: {
+    width: 190,
+    height: 190,
+    zIndex: 1,
+    background: "#b3b3ff",
+  },
+  //Card9 Ends
+
+  //Card10 Starts
+  card10: { top: 60, marginLeft: "-1px" },
+  verticalString10: { height: 150 },
+  imageDiv10: {
+    width: 160,
+    height: 160,
+    zIndex: 1,
+    boxShadow: "0px 0px 60px 0px #dad9d9",
+    borderRadius: "4px",
+
+    // background: "#000080",
+  },
+  //Card10 Ends
+
+  //Card11 Starts
+  card11: { top: 50, marginLeft: "-15px" },
+  verticalString11: { height: 90 },
+  imageDiv11: {
+    width: 120,
+    height: 110,
+    boxShadow: "0px 0px 60px 0px #dad9d9",
+    // background: "#000099",
+  },
+  //Card11 Ends
+};
+
+export default styles;
